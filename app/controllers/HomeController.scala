b42ebe80534debbb53a7629c25a12ac65e95580b
QAcpardeep <cpardeep@qa.com>
2021-02-15 14:10:43 +0000

    QAC-56 QAC-58 QAC-59 QAC-69 QAC-70 QAC-71

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,7 +1,10 @@
 package controllers
 
+import play.api.i18n.Messages.implicitMessagesProviderToMessages
+
 import javax.inject._
 import play.api.mvc._
+
 import scala.util.{Failure, Success}
 import scala.concurrent.ExecutionContext.Implicits.global
 
@@ -11,5 +14,11 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
   def index = Action {
     Ok(views.html.homePage())
   }
+
+  def contactPage = Action {
+    Ok(views.html.contactPage())
+    //Redirect(routes.HomeController.index())
+  }
+
 
 }
