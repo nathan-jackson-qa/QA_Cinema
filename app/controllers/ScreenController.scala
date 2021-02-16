5defc5c450f74ea10bff952dc18e339a780c0275
Ashley-Cameron <AshleyCameron1994@outlook.com>
2021-02-16 09:35:34 +0000

    QAC-53 QAC:54-screens page complete

---
+++ app/controllers/ScreenController.scala
@@ -0,0 +1,12 @@
+package controllers
+
+import play.api.mvc.{AbstractController, ControllerComponents}
+
+import javax.inject.Inject
+
+class ScreenController  @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+
+  def screen = Action {
+    Ok(views.html.screenPage())
+  }
+}
