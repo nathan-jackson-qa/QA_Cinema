2fe61c668b472dbddc708495159aac5e37420f89
QA-SamWilliams <swilliams@academytrainee.com>
2021-02-17 09:27:18 +0000

    QAC-61

--- app/controllers/GalleryController.scala
+++ app/controllers/GalleryController.scala
@@ -1,13 +1,21 @@
 package controllers
 
+import dao.movieDAO
 import play.api.mvc._
 
 import javax.inject._
+import play.api.mvc.{AbstractController, Action, ControllerComponents}
+
+import javax.inject.Inject
+import scala.concurrent.ExecutionContext.Implicits.global
+
 
 @Singleton
 class GalleryController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-  def index = Action {
-    Ok(views.html.whatsOn())
+  def viewAll = Action async  {
+    movieDAO.getAllMovies map {
+      results => Ok(views.html.whatsOn(results))
+    }
   }
 }
