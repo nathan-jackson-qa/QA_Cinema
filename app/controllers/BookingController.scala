9def430a2b0dd799b00c212ef249d654ebb63c43
QAcpardeep <cpardeep@qa.com>
2021-02-17 17:17:10 +0000

    QAC-100

--- app/controllers/BookingController.scala
+++ app/controllers/BookingController.scala
@@ -1,7 +1,7 @@
 package controllers
 
 import dao.{bookingDAO, cinemaDAO, movieDAO}
-import models.bookingForm
+import models.{Cinema, bookingForm}
 import play.api.mvc._
 
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -10,29 +10,19 @@ import javax.inject.{Inject, Singleton}
 @Singleton
 class BookingController @Inject()(cc: ControllerComponents) extends AbstractController(cc) with play.api.i18n.I18nSupport{
 
-  def form() = Action {
+  def form() = Action.async{
     implicit request: Request[AnyContent] =>
-      Ok(views.html.ticketBooking(bookingForm.form))
+      cinemaDAO.getAllCinemas.map{
+        results => Ok(views.html.ticketBooking(bookingForm.form, results))
+      }
   }
 
   def inputBooking() = Action { implicit request: Request[AnyContent] =>
     println(bookingForm.form.bindFromRequest().get)
-    bookingForm.form.bindFromRequest().fold({ formWithErrors => BadRequest(views.html.ticketBooking(formWithErrors))},
+    bookingForm.form.bindFromRequest().fold({ formWithErrors => BadRequest(views.html.ticketBooking(formWithErrors, Seq[Cinema]()))},
       { widget => bookingDAO.add(widget)
         Redirect(routes.PayPalController.index())
     })
   }
-
-  def cinemaAll = Action async {
-    cinemaDAO.getAllCinemas map {
-      results => Ok(views.html.cinemaBooking(results))
-    }
-  }
-
-//  def movieAll = Action async {
-//    movieDAO.getAllMovies map {
-//      results => Ok("woo" ))
-//    }
-//  }
 
 }
