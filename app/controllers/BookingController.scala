25a519f690d77492b3f35501e5f52283a72da74a
Nj147 <njackson@qa.com>
2021-02-18 09:58:51 +0000

    QAC-33 Pulling from multiple tables half implmeneted

--- app/controllers/BookingController.scala
+++ app/controllers/BookingController.scala
@@ -1,15 +1,21 @@
 package controllers
 
-import dao.bookingDAO
+import dao.{bookingDAO, movieDAO}
 import models.bookingForm
 import play.api.mvc._
-
+import scala.concurrent.ExecutionContext.Implicits.global
 import javax.inject.{Inject, Singleton}
+import scala.util.{Failure, Success}
 
 @Singleton
 class BookingController @Inject()(cc: ControllerComponents) extends AbstractController(cc) with play.api.i18n.I18nSupport{
 
   def form() = Action {
+        val data = movieDAO.getMovieActors
+        data onComplete {
+          case Success(value) => println(value)
+          case Failure(error) => error.printStackTrace()
+        }
     implicit request: Request[AnyContent] =>
       Ok(views.html.ticketBooking(bookingForm.form))
   }
--- app/dao/movieDAO.scala
+++ app/dao/movieDAO.scala
@@ -1,6 +1,6 @@
 package dao
 
-import models.{Movie, Movies}
+import models.{Actor, Actors, Movie, Movie_Actors, Movies}
 import slick.jdbc.MySQLProfile.backend.Database
 import slick.jdbc.MySQLProfile.api._
 
@@ -9,13 +9,25 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 object movieDAO {
   lazy val db: Database = Database.forConfig("mysqlDB")
-  lazy val table: TableQuery[Movies] = TableQuery[Movies]
+  lazy val movies: TableQuery[Movies] = TableQuery[Movies]
+  lazy val moviesActors: TableQuery[Movie_Actors] = TableQuery[Movie_Actors]
+  lazy val actors: TableQuery[Actors] = TableQuery[Actors]
 
   def getMovieDetails(id: Int): Future[Seq[Movie]] = {
-    db.run(table.filter(t => t.id === id).result)
+    db.run(movies.filter(t => t.id === id).result)
   }
 
   def getAllMovies: Future[Seq[Movie]] = {
-    db.run(table.result)
+    db.run(movies.result)
+  }
+
+  def getMovieActors: Future[Any] = {
+    val actorsMovies = for {
+      m  <- movies
+      am <- moviesActors if m.id === am.movieID
+      a <- actors if am.actorID === a.id
+    } yield (m, a)
+    db.run(actorsMovies.result)
+
   }
 }
