da01ec64c828992e28b2f332f27b6d6aea59a0da
QAcpardeep <cpardeep@qa.com>
2021-02-22 16:51:57 +0000

    QAC-48

--- app/controllers/DiscussionController.scala
+++ app/controllers/DiscussionController.scala
@@ -15,22 +15,43 @@ class DiscussionController @Inject()(cc: ControllerComponents) extends AbstractC
       results => Ok(views.html.discussionPage(results))
     }
   }
+
   def discForm() = Action {
     implicit request: Request[AnyContent] =>
       Ok(views.html.discussionPage2(discussionForm.form))
   }
 
-  def discFormPost() = Action { implicit request : Request[AnyContent] =>
-      discussionForm.form.bindFromRequest().fold({ formWithErrors => BadRequest(views.html.discussionPage2(formWithErrors))},
-        { widget => discussionDAO.create(widget)
-        Redirect(routes.DiscussionController.index())}
+  def discFormPost() = Action { implicit request: Request[AnyContent] =>
+    discussionForm.form.bindFromRequest().fold({ formWithErrors => BadRequest(views.html.discussionPage2(formWithErrors)) },
+      { widget => {
+        if (split(widget.description) == true || split(widget.title) == true) {
+          discussionDAO.create(Discussion(widget.id, widget.title, widget.description, widget.rating, false))
+        }
+        else {
+          discussionDAO.create(Discussion(widget.id, widget.title, widget.description, widget.rating, true))
+        }
+      }
+        Redirect(routes.DiscussionController.index())
+      }
     )
   }
-  def index2() = Action async {
-    discussionDAO.getReviews map {
-      results => Ok(views.html.admin(results))
+
+  def split(text: String): Boolean = {
+    val words = text.split(" ")
+    var overall = false
+    for (word <- words) {
+      if (check(word) == true) {
+        overall = true;
+      }
     }
+    overall
   }
+
+  def check(word: String): Boolean = {
+    val badWords = scala.io.Source.fromFile("profanityFilter.txt").getLines.flatMap(_.split("\\W+")).toList
+    badWords.contains(word)
+  }
+
 
 }
 
