ff9ceb12fe892ee11260a56532e987c38fd7f4ae
QAcpardeep <cpardeep@qa.com>
2021-02-23 12:37:02 +0000

    QAC-115

---
+++ test/selenium/mainPage.scala
@@ -0,0 +1,68 @@
+package selenium
+import org.scalatest.flatspec
+import org.scalatest.matchers.should
+import org.scalatestplus.selenium.HtmlUnit
+
+class mainPage extends flatspec.AnyFlatSpec with should.Matchers with HtmlUnit {
+
+  val host = "http://localhost:9000/about"
+
+  //Test1
+  "The Header brand image" should "link to the home page" in {
+    go to (host)
+    click on cssSelector(".navbar-brand > img")
+    pageTitle should be ("QA Cinemas : Home")
+  }
+
+  //Test2
+  "The navbar" should "link to all the corresponding pages" in {
+    go to (host)
+    //Home
+    click on linkText("Home")
+    pageTitle should be ("QA Cinemas : Home")
+    //Out Now
+    click on xpath("/html/body/nav/div/div/ul/li[2]/a")
+    pageTitle should be ("QA Cinemas : Out Now")
+    //Coming Soon
+    click on xpath("/html/body/nav/div/div/ul/li[3]/a")
+    pageTitle should be ("QA Cinemas : Coming Soon")
+    //Opening Times
+    click on xpath("/html/body/nav/div/div/ul/li[4]/a")
+    pageTitle should be ("QA Cinemas : Opening Times")
+    //Screens
+    click on xpath("/html/body/nav/div/div/ul/li[5]/a")
+    pageTitle should be ("QA Cinemas : Screens")
+    //Getting There
+    click on xpath("/html/body/nav/div/div/ul/li[6]/a")
+    pageTitle should be ("QA Cinemas : Getting Here")
+    //Classifications
+    click on xpath("/html/body/nav/div/div/ul/li[7]/a")
+    pageTitle should be ("QA Cinemas : Classifications")
+    //Discussions
+    click on xpath("/html/body/nav/div/div/ul/li[8]/a")
+    pageTitle should be ("QA Cinemas : Discussion")
+  }
+
+  //Test3
+  "The footer" should "link to all the corresponding pages" in {
+    go to (host)
+    //Home
+    click on xpath("/html/body/footer/div/div/div[1]/h5/a")
+    pageTitle should be ("QA Cinemas : About Page")
+    //Out Now
+    click on xpath("/html/body/footer/div/div/div[2]/h5[1]/a")
+    pageTitle should be ("QA Cinemas : Contact Page")
+  }
+
+  //Test=3
+  "The search bar" should "present all movies related the keyword" in {
+    go to (host)
+    click on xpath("/html/body/nav/div/div/ul/form/button")
+    webDriver.findElementByXPath("/html/body/nav/div/div/ul/form") sendKeys("Flushed Away")
+    click on xpath("/html/body/nav/div/div/ul/form/button")
+    webDriver.findElementByXPath("/html/body/div/div/div/div[2]/div/h4").getText should be ("Flushed Away")
+
+    //new WebDriverWait(webDriver, 2).until(ExpectedConditions.visibilityOf(webDriver.findElementByXPath("/html/body/nav/div/div/ul/li[2]/ul/li[1]/a")))
+  }
+
+}
