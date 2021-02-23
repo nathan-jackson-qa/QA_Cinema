8e08102a6d096e9cf8c58e602d5451f05033f1ff
Nj147 <njackson@qa.com>
2021-02-23 12:52:24 +0000

    NJ: Added bootstrap error remover

--- test/selenium/abstractTest.scala
+++ test/selenium/abstractTest.scala
@@ -5,5 +5,7 @@ import org.scalatest.matchers.should
 import org.scalatestplus.selenium.HtmlUnit
 
 trait abstractTest extends flatspec.AnyFlatSpec with should.Matchers with HtmlUnit {
-
+  org.slf4j.LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME)
+    .asInstanceOf[ch.qos.logback.classic.Logger]
+    .setLevel(ch.qos.logback.classic.Level.ERROR)
 }
