d1d2e22b148a4763ce151335432f949319d72398
Nj147 <njackson@qa.com>
2021-02-24 10:55:59 +0000

    NJ: Added maps test to getting here

--- test/selenium/gettingHereTest.scala
+++ test/selenium/gettingHereTest.scala
@@ -10,12 +10,22 @@ class gettingHereTest extends abstractTest {
   "The getting here page" should "allow the user to switch between each cinemas details" in {
     go to (host)
     pageTitle should be ("QA Cinemas : Getting Here")
-    assert(webDriver.findElementById("pills-1-tab").isDisplayed)
-    webDriver.findElement(By.id("pills-1-tab")).sendKeys(Keys.ENTER)
+    click on xpath("/html/body/div/div[2]/ul/li[1]/button")
     webDriver.findElementById("cinema-1").getAttribute("style") should be ("color: #D8D4F2")
     click on xpath("/html/body/div/div[2]/ul/li[2]/button")
     webDriver.findElementById("cinema-2").getAttribute("style") should be ("color: #D8D4F2")
     click on xpath("/html/body/div/div[2]/ul/li[3]/button")
     webDriver.findElementById("cinema-3").getAttribute("style") should be ("color: #D8D4F2")
   }
+
+  "The getting here page" should "have google maps embedded with the location of the cinema displayed" in {
+    go to (host)
+    click on xpath("/html/body/div/div[2]/ul/li[1]/button")
+    assert(webDriver.findElementByXPath("//*[@id=\"pills-1\"]/div[2]/iframe").getAttribute("src").contains("maps"))
+    click on xpath("/html/body/div/div[2]/ul/li[2]/button")
+    assert(webDriver.findElementByXPath("//*[@id=\"pills-1\"]/div[2]/iframe").getAttribute("src").contains("maps"))
+    click on xpath("/html/body/div/div[2]/ul/li[3]/button")
+    assert(webDriver.findElementByXPath("//*[@id=\"pills-1\"]/div[2]/iframe").getAttribute("src").contains("maps"))
+  }
+
 }
