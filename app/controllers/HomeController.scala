673b464f96a7fd3d71a694779becdc29cc44cef3
Nj147 <njackson@qa.com>
2021-02-24 15:09:31 +0000

    NJ: Added searches to individual whats on

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,44 +1,39 @@
 package controllers
 
 import dao.movieDAO
-import models.Movies
+import models.{Movie, Movies}
 import play.api.mvc.Results.Ok
 import play.api.mvc._
 
 import javax.inject._
 import play.api.mvc.{AbstractController, Action, ControllerComponents}
-
+import scala.collection.mutable.Set
 import javax.inject.Inject
 import scala.concurrent.ExecutionContext.Implicits.global
 
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-  def viewAll = Action async  {
+  def viewAll = Action async {
     movieDAO.getAllMovies map {
       results => Ok(views.html.homePage(results))
     }
   }
 
   def search(keyword: String) = Action async {
-    movieDAO.searchActors(keyword).flatMap {
-      firstResults =>
-        movieDAO.searchBykeyword(keyword) map {
-          secondResults => {
-            var results = firstResults.toSet ++ secondResults.toSet; Ok(views.html.searchResults(results.toSeq))
-          }
+    var words = keyword.split(" ")
+    var movies: Set[Movie] = Set()
+    for (word <- words) {
+      movieDAO.searchActors(word) map {
+        results => {
+          movies ++= results
         }
+      }
+    }
+    movieDAO.searchBykeyword(keyword) map {
+      secondResults => {
+        var results = movies ++ secondResults.toSet;
+        Ok(views.html.searchResults(results.toSeq))
+      }
     }
   }
-
-//  def viewTen = Action async  {
-//    movieDAO.getReleasedMovies map {
-//      results => Ok(views.html.homePage(results))
-//    }
-//  }
-//
-//  def viewAllUpcoming = Action async  {
-//    movieDAO.getUpcomingMovies map {
-//      results => Ok(views.html.homePage(results))
-//    }
-//  }
 }
